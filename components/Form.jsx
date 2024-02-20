dad637e319c0bc5a89cfcf76ad4ba9f40ccd461a
Najmul Islam Naeem <csenajmulislamnaeem@gmail.com>
2024-02-21 02:44:53 +0600

    First Commit

---
+++ components/Form.jsx
@@ -0,0 +1,167 @@
+"use client";
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import {
+  EmailOutlined,
+  LockOutlined,
+  PersonOutline,
+} from "@mui/icons-material";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { signIn } from "next-auth/react"
+// import {useState} from React; 
+
+const Form = ({ type }) => {
+  // const [show,setShow] = useState('password') ;
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const router = useRouter();
+
+  const onSubmit = async (data) => { 
+    if (type === "register") {
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (res.ok) {
+        router.push("/");
+      }
+
+      if (res.error) {
+        toast.error("Something went wrong");
+      }
+    }
+
+    if (type === "login") {
+      const res = await signIn("credentials", {
+        ...data,
+        redirect: false,
+      })
+
+      if (res.ok) {
+        router.push("/chats");
+      }
+
+      if (res.error) {
+        toast.error("Invalid email or password");
+      }
+    }
+    
+  };
+  const showPass = ()=>{
+   
+    var inputElement = document.getElementById("pass");
+
+      // Check the current type and change it accordingly
+      if (inputElement.type === "text") {
+        inputElement.type = "password";
+      } else {
+        inputElement.type = "text";
+      }
+  }
+
+  return (
+    <div className="auth">
+      <div className="content">
+        <img src="/assets/logo.png" alt="logo" className="logo" />
+
+        <form className="form" onSubmit={handleSubmit(onSubmit)}>
+          {type === "register" && (
+            <div>
+              <div className="input">
+                <input
+                  defaultValue=""
+                  {...register("username", {
+                    required: "Username is required",
+                    validate: (value) => {
+                      if (value.length < 3) {
+                        return "Username must be at least 3 characters";
+                      }
+                    },
+                  })}
+                  type="text"
+                  placeholder="Username"
+                  className="input-field"
+                />
+                <PersonOutline sx={{ color: "#737373" }} />
+              </div>
+              {errors.username && (
+                <p className="text-red-500">{errors.username.message}</p>
+              )}
+            </div>
+          )}
+
+          <div>
+            <div className="input">
+              <input
+                defaultValue=""
+                {...register("email", { required: "Email is required" })}
+                type="email"
+                placeholder="Email"
+                className="input-field"
+              />
+              <EmailOutlined sx={{ color: "#737373" }} />
+            </div>
+            {errors.email && (
+              <p className="text-red-500">{errors.email.message}</p>
+            )}
+          </div>
+
+          <div>
+            <div className="input">
+              <input
+              id='pass'
+                defaultValue=""
+                {...register("password", {
+                  required: "Password is required",
+                  validate: (value) => {
+                    if (
+                      value.length < 5 ||
+                      !value.match(/[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/)
+                    ) {
+                      return "Password must be at least 5 characters and contain at least one special character";
+                    }
+                  },
+                })}
+                type="password"
+                placeholder="Password"
+                className="input-field"
+              />
+             
+             < VisibilityIcon  onClick={showPass} sx={{ color: "#737373" }} />
+            </div>
+            {errors.password && (
+              <p className="text-red-500">{errors.password.message}</p>
+            )}
+          </div>
+
+          <button className="button" type="submit">
+            {type === "register" ? "Join Free" : "Let's Chat"}
+          </button>
+        </form>
+
+        {type === "register" ? (
+          <Link href="/" className="link">
+            <p className="text-center">Already have an account? Sign In Here</p>
+          </Link>
+        ) : (
+          <Link href="/register" className="link">
+            <p className="text-center">Don't have an account? Register Here</p>
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Form;
